c3d2a05568f3f3ae7bde68b8d3be062789fb56b8
Joseph Lira <joogasl@gmail.com>
2026-03-23 15:11:56 -0400

    Removed unnecessary Json marshaling

--- backend/app/controller/Employee_controller.py
+++ backend/app/controller/Employee_controller.py
@@ -1,6 +1,4 @@
 from backend.app.model.Employee_model import get_all_employees
-from backend.app.schemas.Employee_schema import EmployeeResponse
 
 def fetch_all_employees():
-    employees = get_all_employees()
-    return [EmployeeResponse(**emp) for emp in employees]
+    return get_all_employees()

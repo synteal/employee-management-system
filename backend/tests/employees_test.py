59aabffaabb81f78effe2b59d1e1f6285550faa5
Joseph Lira <joogasl@gmail.com>
2026-03-23 16:04:15 -0400

    Added test stubs for remaining employee CRUD.

--- backend/tests/employees_test.py
+++ backend/tests/employees_test.py
@@ -1,3 +1,4 @@
+from backend.app.model.employee_model import get_all_employees
 from fastapi.testclient import TestClient
 from backend.app.main import app
 
@@ -9,6 +10,7 @@ def test_get_employees():
     assert isinstance(response.json(), list)
 
 def test_add_employee():
+    existing_employees = get_all_employees()
     new_employee = {
         "employeeId": "EMP020",
         "name": "Jane Doe",
@@ -23,4 +25,28 @@ def test_add_employee():
     assert response_json["name"] == "Jane Doe"
     assert response_json["role"] == "Data Analyst"
     assert response_json["department"] == "IT"
+
+    new_employees = get_all_employees()
+    assert len(new_employees) == len(existing_employees) + 1
     
+def test_update_employee():
+    response = client.put("/employees/employee/EMP020", json={
+        "role": "Data Scientist",
+    })
+    response_json = response.json()
+    assert response.status_code == 200
+    assert response_json["role"] == "Data Scientist"
+
+def test_delete_employee():
+    response = client.delete("/employees/employee/EMP020")
+    assert response.status_code == 200
+
+def test_get_employee_by_id():
+    response = client.get("/employees/employee/EMP020")
+    assert response.status_code == 200
+    assert response.json()["employeeId"] == "EMP020"
+
+def test_get_employee_by_department():
+    response = client.get("/employees/department/IT")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)

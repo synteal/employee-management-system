6872547c886507526a4b98a6f3dd421845ef8017
Joseph Lira <joogasl@gmail.com>
2026-03-25 09:44:41 -0400

    Algorithm now comes from environment

--- backend/config/auth.py
+++ backend/config/auth.py
@@ -5,7 +5,11 @@ if SECRET_KEY is None:
     logger.error("SECRET_KEY is not set. JWT Token generation cannot occur.")
     raise ValueError("SECRET_KEY is not set.")
     
-ALGORITHM = "HS256"
+ALGORITHM = os.getenv("ALGORITHM")
+if ALGORITHM is None:
+    logger.error("ALGORITHM is not set. JWT Token generation cannot occur.")
+    raise ValueError("ALGORITHM is not set.")
+    
 env_expire = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 if env_expire is None:
     logger.error("ACCESS_TOKEN_EXPIRE_MINUTES is not set. JWT Token lifespan cannot be determined.")

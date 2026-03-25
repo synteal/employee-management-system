293fd90965f6026b30030a3af66548e6c12e0230
Joseph Lira <joogasl@gmail.com>
2026-03-25 14:11:54 -0400

    Added vitest to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+/// <reference types="vitest/config" />
+
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts'
+  },
+});
